1b12685deb8ad74aad465f95d3d83550a8a089c3
Pyzyryab <elgithub_10>
2022-01-15 22:52:36 +0100

    Refactored the project structure. Cleaned the root directory. Writed the basic scaffold for the main class.

---
+++ include/Owl/Owl.hpp
@@ -0,0 +1,22 @@
+﻿/**
+ * @brief The main entry point for the core library of the framework.
+ *
+ * @file WexTest.cpp
+ * @author Alex Vergara (pyzyryab@tutanota.com)
+ * @version 0.1.0
+ * @date 2022-01-15
+ * @copyright Copyright (c) 2022
+ * 
+ * This is the main header file for the whole framework. Where will me modularized and defined the essential
+ * components that will decide the way in how this framework it's created.
+ */
+
+#pragma once
+
+#include <iostream>
+
+class Owl {
+	private:
+
+	public:
+};
---
+++ src/Owl.cpp
@@ -0,0 +1,12 @@
+﻿/**
+ * @brief The implementations for the declared code on the header file
+ *
+ * @file WexTest.cpp
+ * @author Alex Vergara (pyzyryab@tutanota.com)
+ * @version 0.1.0
+ * @date 2022-01-15
+ * @copyright Copyright (c) 2022
+ */
+#include "../include/Owl/Owl.hpp"
+
+using namespace std;
