a52bbab07af5331e856bbdf74e704166ae04f25b
Pyzyryab <elgithub_10>
2022-01-16 17:52:05 +0100

    Trying out CMake with CppUnitTestFramework

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Alex/Desktop/C++/Owl
-# Build directory: C:/Users/Alex/Desktop/C++/Owl/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Alex/Desktop/C++/Owl/test
-# Build directory: C:/Users/Alex/Desktop/C++/Owl/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
